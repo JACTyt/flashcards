753f740a180c9dea654c5ae9ab01fde00a7509c1
Dmytro Maistruk <dima.maystruk@ukr.net>
2025-11-09 17:51:28 +0100

    Multiple cards

--- src/flashcards/Main.java
+++ src/flashcards/Main.java
@@ -5,19 +5,28 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        System.out.println("Input the number of cards: ");
+        int n = Integer.parseInt(sc.nextLine());
 
-        String term = sc.nextLine();
-        System.out.println(term);
+        String[] terms = new String[n];
+        String[] definitions = new String[n];
+        String[] answers = new String[n];
 
-        String definition = sc.nextLine();
-        System.out.println(definition);
-
-        String answer = sc.nextLine();
-        if(definition.equals(answer)) {
-            System.out.println("Your answer is right!");
+        for (int i = 0; i < n; i++) {
+            System.out.println("Card #" + (i+1) + ": ");
+            terms[i] = sc.nextLine();
+            System.out.println("The definition for card #"+(i+1)+": ");
+            definitions[i] = sc.nextLine();
         }
-        else{
-            System.out.println("Your answer is wrong...");
+        for (int i = 0; i < n; i++) {
+            System.out.println("Print the definition of \"" + terms[i] + "\":");
+            answers[i] = sc.nextLine();
+            if(definitions[i].equals(answers[i])){
+                System.out.println("Correct!");
+            }
+            else{
+                System.out.println("Wrong. The right answer is \"" + definitions[i] + "\".");
+            }
         }
     }
 }
