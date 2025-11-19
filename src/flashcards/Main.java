75a7fbe2034c9992adea902053e77e03dca1745d
Dmytro Maistruk <dima.maystruk@ukr.net>
2025-11-19 15:53:01 +0100

    small update

--- src/flashcards/Main.java
+++ src/flashcards/Main.java
@@ -1,5 +1,7 @@
 package flashcards;
 
+import java.util.LinkedHashMap;
+import java.util.Map;
 import java.util.Scanner;
 
 public class Main {
@@ -8,23 +10,55 @@ public class Main {
         System.out.println("Input the number of cards: ");
         int n = Integer.parseInt(sc.nextLine());
 
-        String[] terms = new String[n];
-        String[] definitions = new String[n];
+        String term = "", definition = "";
         String[] answers = new String[n];
+        Map<String, String> dictionary = new LinkedHashMap<String, String>();
 
-        for (int i = 0; i < n; i++) {
-            System.out.println("Card #" + (i+1) + ": ");
-            terms[i] = sc.nextLine();
-            System.out.println("The definition for card #"+(i+1)+": ");
-            definitions[i] = sc.nextLine();
-        }
-        for (int i = 0; i < n; i++) {
-            System.out.println("Print the definition of \"" + terms[i] + "\":");
-            answers[i] = sc.nextLine();
-            if(definitions[i].equals(answers[i])){
-                System.out.println("Correct!");
+        int i = 0;
+        while (i < n) {
+
+            System.out.println("Card #" + (i + 1) + ": ");
+
+            while (true) {
+                term = sc.nextLine();
+                if (dictionary.containsKey(term)) {
+                    System.out.println("The term \"" + term + "\" already exists. Try again:");
+                } else {
+                    break;
+                }
             }
-            else{
+
+            System.out.println("The definition for card #" + (i + 1) + ": ");
+            while (true) {
+                definition = sc.nextLine();
+                if (dictionary.containsValue(definition)) {
+                    System.out.println("The term \"" + definition + "\" already exists. Try again:");
+                } else {
+                    break;
+                }
+            }
+
+            dictionary.put(term, definition);
+            i++;
+        }
+        String[] terms = dictionary.keySet().toArray(new String[0]);
+        String[] definitions = dictionary.values().toArray(new String[0]);
+        for (i = 0; i < n; i++) {
+            String termToAsk = terms[i];
+            System.out.println("Print the definition of \"" + termToAsk + "\":");
+            answers[i] = sc.nextLine();
+            if (dictionary.containsValue(answers[i])) {
+                if (answers[i].equals(definitions[i])) {
+                    System.out.println("Correct!");
+                } else {
+                    for (int j = 0; j < definitions.length; j++) {
+                        if (answers[i].equals(definitions[j]) && i != j) {
+                            System.out.println("Wrong. The right answer is \"" + definitions[i] + "\", but your definition is correct for \"" + terms[j] + "\".");
+                            break;
+                        }
+                    }
+                }
+            } else {
                 System.out.println("Wrong. The right answer is \"" + definitions[i] + "\".");
             }
         }
