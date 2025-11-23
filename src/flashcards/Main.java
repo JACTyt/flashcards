fbf07b7f05ea8fbb39f041ccef1daef0ed1fd9eb
Dmytro Maistruk <dima.maystruk@ukr.net>
2025-11-23 11:53:24 +0100

    implemented actions

--- src/flashcards/Main.java
+++ src/flashcards/Main.java
@@ -1,13 +1,41 @@
 package flashcards;
 
-import java.util.LinkedHashMap;
-import java.util.Map;
-import java.util.Scanner;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.*;
 
 public class Main {
+    static Map<String, String> dictionary = new TreeMap<String, String>();
+    static Scanner sc = new Scanner(System.in);
+
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Input the number of cards: ");
+        while (true) {
+            System.out.println("Input the action (add, remove, import, export, ask, exit):");
+            Scanner sc = new Scanner(System.in);
+            switch (sc.nextLine()) {
+                case "add":
+                    addCard();
+                    break;
+                case "remove":
+                    removeCard();
+                    break;
+                case "import":
+                    importFile();
+                    break;
+                case "export":
+                    exportFile();
+                    break;
+                case "ask":
+                    askCard();
+                    break;
+                case "exit":
+                    System.out.println("Bye bye!");
+                    return;
+            }
+        }
+        /*
         int n = Integer.parseInt(sc.nextLine());
 
         String term = "", definition = "";
@@ -62,5 +90,117 @@ public class Main {
                 System.out.println("Wrong. The right answer is \"" + definitions[i] + "\".");
             }
         }
+
+         */
+    }
+
+    private static void exportFile() {
+        System.out.println("File name:");
+        String fileName = sc.nextLine();
+
+        int savedCount = 0;
+
+        try (FileWriter writer = new FileWriter(fileName)) {
+            for (Map.Entry<String, String> entry : dictionary.entrySet()) {
+                writer.write(entry.getKey() + ":" + entry.getValue() + "\n");
+                savedCount++;
+            }
+            System.out.println(savedCount + " cards have been saved.");
+        } catch (IOException e) {
+            System.out.println("An error occurred while saving the file.");
+        }
+    }
+
+    private static void askCard() {
+        System.out.println("How many times to ask?\n");
+        int count = Integer.parseInt(sc.nextLine());
+
+        Random random = new Random();
+        List<String> terms = new ArrayList<>(dictionary.keySet());
+        for (int i = 0; i < count; i++) {
+            String term = terms.get(random.nextInt(terms.size()));
+            String definition = dictionary.get(term);
+
+            System.out.println("Print the definition of \"" + term + "\":");
+            String answer = sc.nextLine();
+
+            if (answer.equals(definition)) {
+                System.out.println("Correct!");
+            } else if (dictionary.containsValue(answer)) {
+                String otherTerm = dictionary.entrySet().stream()
+                        .filter(entry -> entry.getValue().equals(answer))
+                        .map(Map.Entry::getKey)
+                        .findFirst()
+                        .orElse("");
+                System.out.println("Wrong. The right answer is \"" + definition +
+                        "\", but your answer is correct for \"" + otherTerm + "\".");
+            } else {
+                System.out.println("Wrong. The right answer is \"" + definition + "\".");
+            }
+        }
+    }
+
+    private static void importFile() {
+        System.out.println("File name:");
+        String inputFile = sc.nextLine();
+
+        File file = new File(inputFile);
+        if (!file.exists()) {
+            System.out.println("File not found.");
+            return;
+        }
+
+        int loadedCount = 0;
+
+        try (Scanner fileScanner = new Scanner(file)) {
+            while (fileScanner.hasNextLine()) {
+                String line = fileScanner.nextLine();
+                String[] parts = line.split(":");
+                if (parts.length == 2) {
+                    String term = parts[0];
+                    String definition = parts[1];
+                    dictionary.put(term, definition);
+                    loadedCount++;
+                }
+            }
+            System.out.println(loadedCount + " cards have been loaded.");
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found.");
+        }
+    }
+
+    public static void addCard() {
+        System.out.println("The card:");
+        String term = sc.nextLine();
+
+        if (dictionary.containsKey(term)) {
+            System.out.println("The card \"" + term + "\" already exists.");
+            return;
+        }
+
+        System.out.println("The definition of the card:");
+        String definition = sc.nextLine();
+
+        if (dictionary.containsValue(definition)) {
+            System.out.println("The definition \"" + definition + "\" already exists.");
+            return;
+        }
+
+        System.out.println("The pair (\"" + term + "\":\"" + definition + "\") has been added");
+        dictionary.put(term, definition);
+        System.out.println();
+    }
+
+    public static void removeCard() {
+        System.out.println("Which card?");
+        String term = sc.nextLine();
+
+        if (!dictionary.containsKey(term)) {
+            System.out.println("Can't remove \"" + term + "\": there is no such card.");
+            return;
+        }
+
+        dictionary.remove(term);
+        System.out.println("The card has been removed.");
     }
 }
